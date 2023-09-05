7a140b9ac83f0988a33c915f578f7b80e88e40ba
libx266 <pies_popierdolony@mail.ru>
2023-09-05 21:37:20 +0400

    refucktoring files

--- Neuro/relu.lua
+++
@@ -1,4 +0,0 @@
-function relu(x)
-    local result = math.max(0, x)
-    return math.floor(result + 0.5)
-  end
--- Neuro/sigmoid.lua
+++
@@ -1,9 +0,0 @@
-function sigmoid(x)
-  local sigmoid_range = 15
-  local input_range = 15
-
-  local scaled_x = (2 * sigmoid_range * x / input_range) - sigmoid_range
-  local result = sigmoid_range / (1 + math.exp(-scaled_x))
-  
-  return math.floor(result + 0.5)
-end
